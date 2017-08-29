ddcb766a5b76cb07a076a83d03b9d9d032cba936
Mark Rabins <markrabins@gmail.com>
2017-08-29 16:55:03 -0700

    Set up currency label

--- Window Shopper/Views/CurrencyTextField.swift
+++ Window Shopper/Views/CurrencyTextField.swift
@@ -26,6 +26,7 @@ class CurrencyTextField: UITextField {
         
         layer.cornerRadius = 3.0
         textAlignment = .center
+        clipsToBounds = true
         
         if let pText = placeholder {
             let place = NSAttributedString(string: pText, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
@@ -34,4 +35,20 @@ class CurrencyTextField: UITextField {
             textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
         }
     }
+    
+    override func draw(_ rect: CGRect) {
+        let size: CGFloat = 20
+        let currencyLabel = UILabel(frame: CGRect(x: 5, y: (frame.size.height / 2) - size / 2, width: size, height: size))
+        currencyLabel.backgroundColor = #colorLiteral(red: 0.6668456824, green: 0.6668456824, blue: 0.6668456824, alpha: 0.8003840042)
+        currencyLabel.textAlignment = .center
+        currencyLabel.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        currencyLabel.layer.cornerRadius = 5.0
+        currencyLabel.clipsToBounds = true
+        
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        currencyLabel.text = formatter.currencySymbol
+        addSubview(currencyLabel)
+    }
+    
 }
